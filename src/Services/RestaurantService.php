64bde88ef67ea49db3c794b13e1ce5ea2f851c60
miguel-goyena-cv <miguel_goyena@cuatrovientos.org>
2024-12-19 12:33:09 +0100

    Hacemos un Insert de un restaurante

--- src/Services/RestaurantService.php
+++ src/Services/RestaurantService.php
@@ -14,35 +14,49 @@ class RestaurantService
     private $restaurantesItalianos;
     private $restaurant_types;
 
-    public function __construct(private EntityManagerInterface $entityManager){
+    public function __construct(private EntityManagerInterface $entityManager)
+    {
 
         // Relleno la información de la BBDD
-        $restaurantType1 = new RestaurantTypeDTO(1,"Oriental");
-        $restaurantType2 = new RestaurantTypeDTO(2,"Italiano");
-        $restaurante1 = new RestaurantDTO(10,"La Taggliatella",$restaurantType1);
-        $restaurante2 = new RestaurantDTO(11,"Ñam Sarasate",$restaurantType2);
+        $restaurantType1 = new RestaurantTypeDTO(1, "Oriental");
+        $restaurantType2 = new RestaurantTypeDTO(2, "Italiano");
+        $restaurante1 = new RestaurantDTO(10, "La Taggliatella", $restaurantType1);
+        $restaurante2 = new RestaurantDTO(11, "Ñam Sarasate", $restaurantType2);
         $this->restaurantes = [$restaurante1, $restaurante2];
         $this->restaurantesItalianos = [$restaurante2];
         $this->restaurant_types = [$restaurantType1, $restaurantType2];
-
     }
 
-    public function getItalianRestaurant(): array{
+    public function getItalianRestaurant(): array
+    {
         return $this->restaurantesItalianos;
     }
 
-    public function getAllRestaurants(): array{
+    public function getAllRestaurants(): array
+    {
         return $this->entityManager->getRepository(Restaurant::class)->findAll();
         // return $this->restaurantes;
     }
 
-    public function getAllRestaurtantTypes(): array{
+    public function getAllRestaurtantTypes(): array
+    {
         return $this->restaurant_types;
     }
 
-    public function createRestaurant(RestaurantDTO $nuevoRestaurante) : RestaurantDTO{
-        array_push($this->restaurantes, $nuevoRestaurante);
-        return $nuevoRestaurante;
+    public function createRestaurant(RestaurantDTO $newRestaurant): RestaurantDTO
+    {
+        // Creamos la entidad restaurante
+        $newRestaurantEntity = new Restaurant();
+        $newRestaurantEntity->setName($newRestaurant->name);
+
+        // Le dices a Doctrine que quieres persistit el objeto,, todavia no hace nada
+        $this->entityManager->persist($newRestaurantEntity);
+
+        // Aqui es donde confirmas, asi tienes el concepto de transaccion!!!!
+        $this->entityManager->flush();
+
+        // Fijate que se ha cambiado la entidad con el ID nuevo
+        $newRestaurant->id = $newRestaurantEntity->getId();
+        return $newRestaurant;
     }
-       
 }
